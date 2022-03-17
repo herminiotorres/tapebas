67b50ee60de9803a58cdde9185f15e31e2f2bfa4
Herminio Torres <herminiocesar@gmail.com>
2022-03-16 22:07:44 -0300

    Add credo (#2)
    * add logfile into gitignore
    
    * add license
    
    * add credo as a dependency
    
    * fix credo issues

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -32,8 +32,10 @@ defmodule TapebasWeb.ConnCase do
   end
 
   setup tags do
-    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(Tapebas.Repo, shared: not tags[:async])
-    on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
+    alias Ecto.Adapters.SQL.Sandbox
+
+    pid = Sandbox.start_owner!(Tapebas.Repo, shared: not tags[:async])
+    on_exit(fn -> Sandbox.stop_owner(pid) end)
     {:ok, conn: Phoenix.ConnTest.build_conn()}
   end
 end
