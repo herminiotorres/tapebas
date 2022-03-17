423fafe17aaf83551b3a6b7992931324219dd45e
Herminio Torres <herminiocesar@gmail.com>
2022-03-16 22:06:19 -0300

    add credo as a dependency

--- mix.exs
+++ mix.exs
@@ -49,9 +49,10 @@ defmodule Tapebas.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      
+
       # additional deps
-      {:phoenix_profiler, "~> 0.1.0"}
+      {:phoenix_profiler, "~> 0.1.0"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
   end
 
