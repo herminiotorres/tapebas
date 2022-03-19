3ef78a56ef91cb7440fb46ec93e5b2e55a96f14d
Herminio Torres <herminiocesar@gmail.com>
2022-03-19 09:14:40 -0300

    add talks belongs to events

---
+++ lib/tapebas/events/talk.ex
@@ -0,0 +1,26 @@
+defmodule Tapebas.Events.Talk do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "talks" do
+    field :speaker, :string
+    field :title, :string
+    field :type, Ecto.Enum, values: [:keynote, :general, :beginner, :advanced], default: :general
+    belongs_to :event, Tapebas.Events.Event, foreign_key: :event_id
+
+    timestamps()
+  end
+
+  @fields ~w(title type speaker event_id)a
+  @required_fields ~w(title type speaker event_id)a
+
+  @doc false
+  def changeset(talk, attrs) do
+    talk
+    |> cast(attrs, @fields)
+    |> validate_required(@required_fields)
+    |> validate_length(:title, min: 10)
+    |> validate_length(:speaker, min: 2)
+    |> foreign_key_constraint(:user_id)
+  end
+end
--- test/tapebas/events_test.exs
+++ test/tapebas/events_test.exs
@@ -1,13 +1,13 @@
 defmodule Tapebas.EventsTest do
   use Tapebas.DataCase
 
+  import Tapebas.EventsFixtures
+
   alias Tapebas.Events
+  alias Tapebas.Events.Event
+  alias Tapebas.Events.Talk
 
   describe "events" do
-    alias Tapebas.Events.Event
-
-    import Tapebas.EventsFixtures
-
     @invalid_attrs %{slug: nil, title: nil, description: nil}
 
     test "list_events/0 returns all events" do
@@ -21,7 +21,8 @@ defmodule Tapebas.EventsTest do
     end
 
     test "create_event/1 with valid data creates a event" do
-      valid_attrs = %{title: "some title", description: "some description"}
+      user = Tapebas.AccountsFixtures.user_fixture()
+      valid_attrs = %{title: "some title", description: "some description", user_id: user.id}
 
       assert {:ok, %Event{} = event} = Events.create_event(valid_attrs)
       assert event.slug == "some-title"
@@ -57,6 +58,72 @@ defmodule Tapebas.EventsTest do
     test "change_event/1 returns a event changeset" do
       event = event_fixture()
       assert %Ecto.Changeset{} = Events.change_event(event)
+    end
+  end
+
+  describe "talks" do
+    @invalid_attrs %{speaker: nil, title: nil, type: nil}
+
+    test "list_talks/0 returns all talks" do
+      talk = talk_fixture()
+      assert Events.list_talks() == [talk]
+    end
+
+    test "get_talk!/1 returns the talk with given id" do
+      talk = talk_fixture()
+      assert Events.get_talk!(talk.id) == talk
+    end
+
+    test "create_talk/1 with valid data creates a talk" do
+      event = Tapebas.EventsFixtures.event_fixture()
+
+      valid_attrs = %{
+        speaker: "some speaker",
+        title: "some title",
+        type: "general",
+        event_id: event.id
+      }
+
+      assert {:ok, %Talk{} = talk} = Events.create_talk(valid_attrs)
+      assert talk.speaker == "some speaker"
+      assert talk.title == "some title"
+      assert talk.type == :general
+    end
+
+    test "create_talk/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Events.create_talk(@invalid_attrs)
+    end
+
+    test "update_talk/2 with valid data updates the talk" do
+      talk = talk_fixture()
+
+      update_attrs = %{
+        speaker: "some updated speaker",
+        title: "some updated title",
+        type: "general"
+      }
+
+      assert {:ok, %Talk{} = talk} = Events.update_talk(talk, update_attrs)
+      assert talk.speaker == "some updated speaker"
+      assert talk.title == "some updated title"
+      assert talk.type == :general
+    end
+
+    test "update_talk/2 with invalid data returns error changeset" do
+      talk = talk_fixture()
+      assert {:error, %Ecto.Changeset{}} = Events.update_talk(talk, @invalid_attrs)
+      assert talk == Events.get_talk!(talk.id)
+    end
+
+    test "delete_talk/1 deletes the talk" do
+      talk = talk_fixture()
+      assert {:ok, %Talk{}} = Events.delete_talk(talk)
+      assert_raise Ecto.NoResultsError, fn -> Events.get_talk!(talk.id) end
+    end
+
+    test "change_talk/1 returns a talk changeset" do
+      talk = talk_fixture()
+      assert %Ecto.Changeset{} = Events.change_talk(talk)
     end
   end
 end
