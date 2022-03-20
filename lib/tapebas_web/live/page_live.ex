c401e77830124cc41ad4d95dd18323d6ef464ee6
Cristine Guadelupe <cristineguadelupe@me.com>
2022-03-20 20:21:25 -0300

    Home page - hero section (#15)

--- lib/tapebas_web/live/page_live.ex
+++ lib/tapebas_web/live/page_live.ex
@@ -7,18 +7,4 @@ defmodule TapebasWeb.PageLive do
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
-
-  @impl true
-  def render(assigns) do
-    ~H"""
-    <section class="w-1/2 mx-auto">
-      <div class="p-6 mb-2 bg-white border border-gray-200 shadow-md rounded-md">
-        <header class="w-2/5 mx-auto mb-12 text-center">
-          <h1 class="mb-5 text-xl font-semibold text-gray-500">no events</h1>
-          <p class="text-gray-500">Welcome to my Tapebas page. Please follow the instructions to add an event.</p>
-        </header>
-      </div>
-    </section>
-    """
-  end
 end
